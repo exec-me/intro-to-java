0b63f1486f65ea83b21091260a5cbf7949f31db6
exec-me <iaroslav.belscky@yandex.ru>
2025-05-01 15:26:46 +0300

    Added practice 1, task 5

---
+++ prac1t5.java
@@ -0,0 +1,8 @@
+public class prac1t5 {
+    public static void main(String[] args) {
+        System.out.println("Args:");
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("Arg " + (i + 1) + ": " + args[i]);
+        }
+    }
+}
